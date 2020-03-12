1c82b716d290772753af68cbb77bebb3fd63ea8a
nordize <panupong.wong@gmail.com>
2020-03-12 17:50:14 -0400

    finish ass2.2

--- src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
+++ src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
@@ -12,7 +12,7 @@ public class CaesarShiftCipher {
         for(int i = 0; i < length; i++){
             currentChar = message.charAt(i);
            
-            sb.append(currentChar);
+            //sb.append(currentChar);
             if (currentChar > 'z' || currentChar < 'a') {
                 return "invalid";
             } else if ((char) (currentChar + shift) > 'z') {
@@ -20,6 +20,7 @@ public class CaesarShiftCipher {
             } else if ((char) (currentChar + shift) < 'a'){
                 currentChar = (char) (currentChar + 26);
             }
+            //sb.append((char) (currentChar + shift));
             sb.append((char) (currentChar + shift));
         }
 
--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,38 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import tudelft.ghappy.GHappy;
+
 public class CaesarShiftCipherTest {
 
+    @Test
+    public void test1(){
+        CaesarShiftCipher C_shift = new CaesarShiftCipher();
+
+        String result = C_shift.CaesarShiftCipher("abc",3);
+        Assertions.assertEquals("def",result);
+    }
+
+    @Test
+    public void test2(){
+        CaesarShiftCipher C_shift = new CaesarShiftCipher();
+
+        String result = C_shift.CaesarShiftCipher("xyz",3);
+        Assertions.assertEquals("abc",result);
+    }
+
+    @ParameterizedTest(name = "string={0},result={1}")
+    @CsvSource({
+            "java,lcxc", "hungry,jwpita", "blog,dnqi"
+    })
+    public void test_automation1(String str,String expected_result) {
+        CaesarShiftCipher C_shift = new CaesarShiftCipher();
+
+        String result = C_shift.CaesarShiftCipher(str,2);
+
+        Assertions.assertEquals(expected_result,result);
+    }
 }
